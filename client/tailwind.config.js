4d253e9db5fcf02949c2df3c3893199e451bbe93
Siddhant-Mahato <siddhantmahato252@gmail.com>
2024-07-10 18:48:50 +0530

    created header section

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,8 +1,13 @@
+const flowbite = require("flowbite-react/tailwind");
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    flowbite.content(),
+    "./index.html"
+  ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [flowbite.plugin(),],
 };
