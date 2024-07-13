ae8c0342abb45e5c993498f31d28b1493ebe416d
Siddhant-Mahato <siddhantmahato252@gmail.com>
2024-07-13 16:33:37 +0530

    Complete DarkMode

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,13 +1,18 @@
-import { Button, Navbar, TextInput } from 'flowbite-react'
+import { Avatar, Button, Dropdown, Navbar, TextInput } from 'flowbite-react'
 import React from 'react'
 import { Link, useLocation } from 'react-router-dom'
 import { IoSearchOutline } from "react-icons/io5";
-import { FaMoon } from "react-icons/fa";
+import { FaMoon , FaSun } from "react-icons/fa";
+import { useSelector, useDispatch } from "react-redux";
+import { toggleTheme } from '../redux/theme/themeSlice';
 
 
 const Header = () => {
 
     const path = useLocation().pathname;
+    const dispatch = useDispatch();
+    const { currentUser } = useSelector((state) => state.user);
+    const {theme} = useSelector((state) => state.theme);
 return (
     <Navbar className='border-b-2'>
         <Link to="/" className='self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white' >
@@ -31,15 +36,52 @@ return (
 
         <div className='flex gap-2 md:order-2'>
 
-            <Button className='w-12 h-10 hidden sm:inline' color="gray" pill>
-                <FaMoon />
+            <Button className='w-12 h-10 hidden sm:inline' color="gray" pill onClick={() => dispatch(toggleTheme())}>
+                { theme === 'light' ? <FaSun/> : <FaMoon/>}
             </Button>
 
-            <Link to="/sign-in">
-                <Button gradientDuoTone="purpleToBlue" outline>
-                    Sign In
-                </Button>
-            </Link>
+            {
+                currentUser ? (
+                    
+                    <Dropdown
+                        arrowIcon={false}
+                        inline
+                        label={ 
+                            <Avatar 
+                                alt='user'
+                                img={currentUser.profilePicture}
+                                rounded
+                            />
+                        }
+                    >
+                        
+                        <Dropdown.Header>
+                            <span className='block text-sm '>@{ currentUser.username }</span>
+                            <span className='block text-sm font-medium truncate'>{ currentUser.email }</span>
+                        </Dropdown.Header>
+
+                        <Link to={"/dashboard?tab=profile"} >
+                            <Dropdown.Item>Profile</Dropdown.Item>
+                        </Link>
+
+                        <Dropdown.Divider />
+                        
+                        <Dropdown.Item>
+                            Sign Out
+                            {/* onClick={() => firebase.auth().signOut()} */}
+                        </Dropdown.Item>
+                    </Dropdown>
+                    
+                ) : (
+                    <Link to="/sign-in">
+                        <Button gradientDuoTone="purpleToBlue" outline>
+                            Sign In
+                        </Button>
+                    </Link>
+                )
+            }
+
+            
 
             <Navbar.Toggle/>
 
