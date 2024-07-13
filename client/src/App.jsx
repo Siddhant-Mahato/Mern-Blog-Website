0ff3625854a7343f3e9a2b0dd2b8f9ad455e2f4e
Siddhant-Mahato <siddhantmahato252@gmail.com>
2024-07-13 18:04:02 +0530

    Make DashBoard Private

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,22 +9,25 @@ import Projects from './pages/Projects';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import FooterCom from './components/Footer';
+import PrivateRoute from './components/PrivateRoute';
 
 const App = () => {
   return (
     <BrowserRouter>
-      <Header/>
+      <Header />
       <Routes>
-        <Route path='/' element={<Home/>} />
-        <Route path='/about' element={<About />} />
-        <Route path='/sign-in' element={<SignIn/>} />
-        <Route path='/sign-up' element={<SignUp/>} />
-        <Route path='/dashboard' element={<Dashboard/>} />
-        <Route path='/projects' element={<Projects/>} />
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route element={<PrivateRoute />}>
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Route>
+        <Route path="/projects" element={<Projects />} />
       </Routes>
-      <FooterCom/>
+      <FooterCom />
     </BrowserRouter>
-  )
+  );
 }
 
 export default App
