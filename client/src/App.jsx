35a112b2bb9abcb73c7ebf4b8f78a1747d01c6ad
Siddhant-Mahato <siddhantmahato252@gmail.com>
2024-07-17 20:52:35 +0530

    Complete Post page Functionalty

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -13,6 +13,7 @@ import PrivateRoute from './components/PrivateRoute';
 import OnlyAdminPrivateRoute from './components/OnlyAdminPrivateRoute';
 import CreatePost from './pages/CreatePost';
 import UpdatePost from './pages/UpdatePost';
+import PostPage from './pages/PostPage';
 
 const App = () => {
   return (
@@ -33,11 +34,17 @@ const App = () => {
         </Route>
 
         <Route element={<OnlyAdminPrivateRoute />}>
+          
           <Route path="/create-post" element={<CreatePost />} />
+
           <Route path="/update-post/:postId" element={<UpdatePost />} />
+
         </Route>
 
         <Route path="/projects" element={<Projects />} />
+        
+        <Route path="/post/:postSlug" element={<PostPage />} />
+
       </Routes>
 
       <FooterCom />
